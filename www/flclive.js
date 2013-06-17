7940fd405f9a755c99c4ec915ba673425271482a
Joshua Jackson <jj@anti-exe.com>
2013-06-17 17:25:34 -0500

    Externalizing css and javascript

--- www/flclive.js
+++ www/flclive.js
@@ -20,52 +20,6 @@ fetchJSONFile('http://www.flcbranson.org/api/livebroadcast', function(data){
 	// alert(data.datetime + ', ' + data.status);
 	if (data.status == 'flcb' || data.status == 'flcs') {
 		window.location = "http://www.flcbranson.org/liveapp";
-	} else {
-		document.write('<!DOCTYPE html>\n\
-<html>\n\
-<head>\n\
-<meta charset="utf-8">\n\
-<title>FLC Live</title>\n\
-<link rel="stylesheet" href="flclive.css">\n\
-</head>\n\
-<body>\n\
-<header>\n\
-	<h1><abbr title="Faith Life Church">FLC</abbr> Live</h1>\n\
-</header>\n\
-<div id="boxes">\n\
-	<section id="livebroadcasts" class="box">\n\
-		<h2>Live Broadcasts</h2>\n\
-		<dl>\n\
-			<dt>Sunday</dt>\n\
-			<dd>9:00 AM &amp; 11:00 AM Central</dd>\n\
-			<dt>Friday</dt>\n\
-			<dd>6:30 PM Central</dd>\n\
-		</dl>\n\
-	</section>\n\
-	<section id="rebroadcasts" class="box">\n\
-		<h2>Rebroadcasts</h2>\n\
-		<dl>\n\
-			<dt>Most Recent, Full Service</dt>\n\
-			<dd><a href="javascript:sundayRebroadcast();">Sunday</a></dd>\n\
-			<dd><a href="javascript:fridayRebroadcast();">Friday</a></dd>\n\
-		</dl>\n\
-	</section>\n\
-	<section id="featuredseries" class="box">\n\
-		<h2>Featured Series</h2>\n\
-		<div id="featuredseriestitle"></div>\n\
-		<div id="sermons"></div>\n\
-	</section>\n\
-	<section id="externallinks" class="box">\n\
-		<h2>External Links <small>opens in system browser</small></h2>\n\
-		<ul>\n\
-			<li><a href="javascript:redirectToSystemBrowser(\'http://www.flcbranson.org/\');">Official <abbr title="Faith Life Church">FLC</abbr> Website</a></li>\n\
-			<li><a href="javascript:redirectToSystemBrowser(\'https://faithlifechurchbranson.thankyou4caring.org/\');">Sow to <abbr title="Faith Life Church">FLC</abbr></a></li>\n\
-		</ul>\n\
-	</section>\n\
-</div>\n\
-<div id="pointlessdivforbackgroundimage"></div>\n\
-</body>\n\
-</html>\n');
 	}
 });
 
@@ -74,8 +28,6 @@ fetchJSONFile('http://www.flcbranson.org/api/featuredseries', function(data) {
 	// do something with your data
 	// alert(JSON.stringify(data));
 	// alert(data.title + ', ' + data.camelcase);
-	var featuredseries_title = data.title;
-	var featuredseries_camelcase = data.camelcase;
 	document.getElementById('featuredseriestitle').innerHTML = '<a href="#">' + data.title + '</a>';
 	loadXML('http://www.flcbranson.org/rss/' + data.camelcase + '.xml');
 });
@@ -176,12 +128,3 @@ function redirectToSystemBrowser(url) {
 		var ref = window.open(encodeURI(url), '_system', 'location=yes');
 	}
 }
-
-/*
-// Google analytics plugin
-var gaPlugin;
-function onDeviceReady() {
-    gaPlugin = window.plugins.gaPlugin;
-    gaPlugin.init(successHandler, errorHandler, "UA-39575525-1", 10);
-}
-*/
