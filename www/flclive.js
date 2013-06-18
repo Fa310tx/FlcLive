1b7e73004aadcc4801069459666cdf20bf5bc713
Joshua Jackson <jj@anti-exe.com>
2013-06-18 09:59:08 -0500

    move google analytics out of flclive.js

--- www/flclive.js
+++ www/flclive.js
@@ -128,13 +128,3 @@ function redirectToSystemBrowser(url) {
 		var ref = window.open(encodeURI(url), '_system', 'location=yes');
 	}
 }
-
-// google analytics
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-39575525-1']);
-_gaq.push(['_trackPageview']);
-(function() {
-	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
